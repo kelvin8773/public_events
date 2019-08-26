35c0fbcb70d69eb05bdbf5b48eeb74722d03e4ef
Kelvin Liang <kelvin9877@gmail.com>
2019-08-27 00:16:05 +0800

    clean up cache and add one more test

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -12,15 +12,15 @@ RSpec.describe 'root path test', type: :feature do
     expect(page).to have_content('This Emails is not register!')
   end
 
-  # scenario 'login Success Test' do
-  #   visit login_path
+  scenario 'login Success Test' do
+    visit login_path
 
-  #   fill_in 'session[email]', with: 'kelvin@example.com'
-  #   fill_in 'session[password]', with: 'foobar'
-  #   click_button 'commit'
+    fill_in 'session[email]', with: 'kelvin@abc.com'
+    fill_in 'session[password]', with: 'password'
+    click_button 'commit'
 
-  #   expect(page).to have_content('You are logged in!')
-  # end
+    expect(page).to have_content('You are logged in!')
+  end
 
   
 
