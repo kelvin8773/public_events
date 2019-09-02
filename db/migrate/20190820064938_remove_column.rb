bcdbb8a8115989bee5691628c5236d615324cd5a
Kelvin Liang <kelvin9877@gmail.com>
2019-09-02 23:25:13 +0800

    remove unuse migration files

--- db/migrate/20190820064938_remove_column.rb
+++
@@ -1,4 +0,0 @@
-class RemoveColumn < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20190820071030_remove_creator_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCreatorFromEvents < ActiveRecord::Migration[5.2]
-  def change
-    # remove_column :events, :creator, :string
-  end
-end
